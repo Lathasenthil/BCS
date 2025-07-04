dccee3ce86d249791b644ce5a5e689a2bdca4451
Lathasenthil <lathapriyabala@gmail.com>
2025-07-04 20:24:44 +0530

    Add files via upload

---
+++ 04_task (1).js
@@ -0,0 +1,6 @@
+var Radi = window.prompt("Enter a Radius Value")
+//var areas = (Radi * Radi) * 3.14159
+var areas = Radi**2 * 3.14159
+
+
+console.log(`The Area for given ${Radi}cm radius is ${areas}cm²`)
